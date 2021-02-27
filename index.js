d35e00641c7fdf202d16222a0352512f07a94a3b
Moisés Soto <musoto96@gmail.com>
2021-02-27 16:05:09 -0600

    Cambios a index css

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8561;
+const PORT = 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
